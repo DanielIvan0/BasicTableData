eb03637da86be874499130324bf381541553d87f
Daniel Ivan <itorres@cistem.com.mx>
2023-01-13 10:54:21 -0800

    hotfix: typo solved

--- src/Core/use_cases/setters.js
+++ src/Core/use_cases/setters.js
@@ -1,10 +1,10 @@
 export function _setNumberOfRows(numberOfRows) {
-	const { first: firstRecordIndex } = this._getPageIndex(this._currentPage);
+	const { i: firstRecordIndex } = this._getPageIndex(this._currentPage);
 	const proportionality = firstRecordIndex / this._data.length;
 
 	this._numberOfRows = numberOfRows;
-	this._numberOfPages = Math.ceil(this._data.length / this.numberOfRows);
-	this._currentPage = Math.ceil(proportionality * this._numberOfPages);
+	this._numberOfPages = Math.ceil(this._data.length / this._numberOfRows) || 1;
+	this._currentPage = Math.ceil(proportionality * this._numberOfPages) || 1;
 }
 
 export function _setCurrentPage(page) { this._currentPage = page; }
